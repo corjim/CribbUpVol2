634b663930d99e6aedcb54b08cb7d0fe04f17def
MartinOrji <martin.orji.jnr@gmail.com>
2025-04-11 17:56:23 -0500

    fix ports for deployment

--- backend/config.js
+++ backend/config.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-const PORT = process.env.REACT_APP_BASE_URL
+const PORT = process.env.PORT
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 // Use dev database, testing database, or via env var, production database
--- frontend/cribbup/src/Api/CribbupApi.js
+++ frontend/cribbup/src/Api/CribbupApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-const API_BASE_URL = process.env.REACT_APP_BASE_URL
+const API_BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 class CribbUpApi {
 
